d2a36b6cf5ddd58b77dd03c2cce0e7a9cf3a303a
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2021-09-29 11:51:07 +0000

    Update motors.py

--- motors.py
+++ motors.py
@@ -4,15 +4,15 @@ from gpiozero import Robot
 motor1 = Motor(4, 14)
 motor2 = Motor(17, 27)
 
-// motor1.forward()
-// motor2.backward()
+# motor1.forward()
+# motor2.backward()
 
 
 motor1.forward(0.5)
 motor2.backward(0.5)
 
-// motor1.forward()
-// motor2.backward()
+# motor1.forward()
+# motor2.backward()
 while True:
     sleep(5)
     motor1.reverse()
