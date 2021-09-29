fb9d232cb2d44c6d410b97a8765c9821b4c2f9b1
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2021-09-29 11:49:57 +0000

    Create motors.py

---
+++ motors.py
@@ -0,0 +1,22 @@
+from gpiozero import Motor
+from gpiozero import Robot
+
+motor1 = Motor(4, 14)
+motor2 = Motor(17, 27)
+
+// motor1.forward()
+// motor2.backward()
+
+
+motor1.forward(0.5)
+motor2.backward(0.5)
+
+// motor1.forward()
+// motor2.backward()
+while True:
+    sleep(5)
+    motor1.reverse()
+    motor2.reverse()
+    
+motor1.stop()
+motor2.stop()
