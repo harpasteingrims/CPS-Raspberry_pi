c58d71aed6fe04eed5a16f0d2bb7a2c470b30b4a
harpasteingrims <54943088+harpasteingrims@users.noreply.github.com>
2021-09-29 11:54:50 +0000

    Update motors.py

--- motors.py
+++ motors.py
@@ -1,15 +1,18 @@
 from gpiozero import Motor
 from gpiozero import Robot
 
-motor1 = Motor(4, 14)
-motor2 = Motor(17, 27)
+robby = Robot(left=(7,8), right=(9,10))
+robby.forward()
+
+#motor1 = Motor(4, 14)
+#motor2 = Motor(17, 27)
 
 # motor1.forward()
 # motor2.backward()
 
 
-motor1.forward(0.5)
-motor2.backward(0.5)
+#motor1.forward(0.5)
+#motor2.backward(0.5)
 
 # motor1.forward()
 # motor2.backward()
