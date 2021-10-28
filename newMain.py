6587665c5d74a81dad89a5f6a84f76636ee4d67a
harpasteingrims <harpastein@gmail.com>
2021-10-28 18:05:12 +0000

    main

--- newMain.py
+++ newMain.py
@@ -140,7 +140,7 @@ signal.signal(signal.SIGINT, signal_handler)
 print("Hello, world!")
 
 # instantiate a PID controller with kp=20, ki=4, kd=2.5, and setpoint=0
-pid_controller = simple_pid.PID(20, 0, 0, setpoint=0)
+pid_controller = simple_pid.PID(20, 4, 2, setpoint=0)
 
 base_throttle = 50
 offset = 1 # the motors do not have equal power, so we offset the throttle slightly to drive in a straight line
